d534a19346eb532bc7a656acd247120c93600f50
smithbrianscott <smithbrianscott23@gmail.com>
2023-07-15 10:08:03 -0400

    install bootstrap, add sample card to home

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pages',
   styleUrls: ['./pages.component.scss'],
-  template: `<app-header></app-header><router-outlet></router-outlet>`,
+  template: `<app-header></app-header>
+    <div class="body-content"><router-outlet></router-outlet></div>`,
 })
 export class PagesComponent implements OnInit {
   constructor() {}
