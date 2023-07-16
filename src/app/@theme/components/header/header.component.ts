226359944882750c5db1fca8750853dd6bf64a8f
Michael Thew <86509688+MichaelAThew@users.noreply.github.com>
2023-07-16 15:47:02 +0000

    set isDarkMode from service on start

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -5,6 +5,7 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
+import { ToggleDarkmodeService } from 'src/app/@core/services/toggle-darkmode.service';
 import { ToggleSidebarService } from 'src/app/@core/services/toggle-sidebar.service';
 
 @Component({
@@ -13,7 +14,7 @@ import { ToggleSidebarService } from 'src/app/@core/services/toggle-sidebar.serv
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit, AfterViewInit {
-  isDarkModeTheme: boolean = false;
+  isDarkMode = false;
   @ViewChild('darkModeSwitch', { read: ElementRef }) element:
     | ElementRef
     | undefined;
@@ -23,7 +24,15 @@ export class HeaderComponent implements OnInit, AfterViewInit {
   moon =
     'M12 21q-3.75 0-6.375-2.625T3 12q0-3.75 2.625-6.375T12 3q.2 0 .425.013 .225.013 .575.038-.9.8-1.4 1.975-.5 1.175-.5 2.475 0 2.25 1.575 3.825Q14.25 12.9 16.5 12.9q1.3 0 2.475-.463T20.95 11.15q.025.3 .038.488Q21 11.825 21 12q0 3.75-2.625 6.375T12 21Zm0-1.5q2.725 0 4.75-1.687t2.525-3.963q-.625.275-1.337.412Q17.225 14.4 16.5 14.4q-2.875 0-4.887-2.013T9.6 7.5q0-.6.125-1.287.125-.687.45-1.562-2.45.675-4.062 2.738Q4.5 9.45 4.5 12q0 3.125 2.188 5.313T12 19.5Zm-.1-7.425Z';
 
-  constructor(private toggleSidbarService: ToggleSidebarService) {}
+  constructor(
+    private toggleSidbarService: ToggleSidebarService,
+    private toggleDarkModeService: ToggleDarkmodeService
+  ) {
+    this.toggleDarkModeService.initTheme();
+    this.isDarkMode = this.toggleDarkModeService.isDarkMode();
+  }
+
+  ngOnInit() {}
 
   ngAfterViewInit(): void {
     if (this.element) {
@@ -37,14 +46,11 @@ export class HeaderComponent implements OnInit, AfterViewInit {
   }
 
   toggleDarkMode() {
-    this.isDarkModeTheme = !this.isDarkModeTheme;
-    console.log('toggleDarkMode', this.isDarkModeTheme);
+    // this.toggleDarkModeService.toggleValue();
+    this.isDarkMode = this.toggleDarkModeService.isDarkMode();
   }
 
-  ngOnInit() {}
-
   toggleSideNav() {
-    console.log('toggleSideNav');
     this.toggleSidbarService.toggleValue();
   }
 }
