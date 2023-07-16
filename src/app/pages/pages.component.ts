7fd7cc95ef25ff4b3dcc8fc5e1d0d2baee598fc4
Michael Thew <thewm@protonmail.com>
2023-07-15 20:28:27 -0400

    app-sidebar into pages component

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pages',
   styleUrls: ['./pages.component.scss'],
-  template: `<app-header></app-header>
-    <div class="body-content"><router-outlet></router-outlet></div>`,
+  template: `<app-sidebar></app-sidebar>`,
 })
 export class PagesComponent implements OnInit {
   constructor() {}
