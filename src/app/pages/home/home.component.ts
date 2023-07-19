b23541ea27c6928d2d91437a652f7594c030bdd6
Michael Thew <thewm@protonmail.com>
2023-07-19 11:53:21 -0400

    clean and update home style

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { LoginResponse, OidcSecurityService } from 'angular-auth-oidc-client';
 import { ThemeService } from 'src/app/@theme/theme.service';
 
 @Component({
@@ -9,56 +7,9 @@ import { ThemeService } from 'src/app/@theme/theme.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor(
-    private themeService: ThemeService,
-    private oidcSecurityService: OidcSecurityService,
-    private httpClient: HttpClient
-  ) {}
+  constructor(private themeService: ThemeService) {}
 
   ngOnInit() {
-    this.themeService.onThemeChange().subscribe((value) => {
-      console.log(value);
-    });
-
-    this.oidcSecurityService
-      .checkAuth()
-      .subscribe((loginResponse: LoginResponse) => {
-        const { isAuthenticated, userData, accessToken, idToken, configId } =
-          loginResponse;
-
-        console.log(loginResponse);
-      });
-  }
-
-  login() {
-    this.oidcSecurityService.authorize();
-  }
-
-  checkAuth() {
-    this.oidcSecurityService
-      .checkAuth()
-      .subscribe(({ isAuthenticated, userData, accessToken }) => {
-        console.log('app authenticated', isAuthenticated);
-        console.log('user', userData);
-        console.log(`Current access token is '${accessToken}'`);
-      });
-  }
-
-  getP1User() {
-    // this.oidcSecurityService.getAccessToken().subscribe((user) => {
-    //   console.log(user);
-    // });
-    let url = `https://api.pingone.com/v1/environments/384e8ed9-73c7-417a-aab9-fcfe8a961e76/users/96f64ca7-e81b-4545-adf7-480f67525016?expand=population`;
-    this.httpClient.get(url).subscribe((res) => {
-      console.log(res);
-    });
-  }
-
-  protectedAPI() {
-    this.httpClient
-      .get('http://localhost/project/api/admin')
-      .subscribe((res) => {
-        console.log(res);
-      });
+    this.themeService.onThemeChange().subscribe((value) => {});
   }
 }
