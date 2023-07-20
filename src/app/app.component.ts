17ff7517866e00c0affaa42a129c2456200ebea8
smithbrianscott <smithbrianscott23@gmail.com>
2023-07-19 20:39:58 -0400

    remove app component styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   template: `<router-outlet></router-outlet>`,
-  styleUrls: ['./app.component.scss'],
   standalone: true,
   imports: [RouterOutlet],
 })
